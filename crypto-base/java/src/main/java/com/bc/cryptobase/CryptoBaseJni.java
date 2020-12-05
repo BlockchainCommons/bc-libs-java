69dc641c938f4fe97731da83ce8fa9fe00cce4e7
Hiếu Phạm <hieupham@bitmark.com>
2020-12-05 23:29:27 +0700

    cleanup crypto-base

--- crypto-base/java/src/main/java/com/bc/cryptobase/CryptoBaseJni.java
+++ crypto-base/java/src/main/java/com/bc/cryptobase/CryptoBaseJni.java
@@ -6,20 +6,20 @@ class CryptoBaseJni {
         System.loadLibrary("bc-crypto-base-jni");
     }
 
-    public static native void sha256_raw(byte[] message, byte[] digest);
+    static native void sha256_raw(byte[] message, byte[] digest);
 
-    public static native void sha512_raw(byte[] message, byte[] digest);
+    static native void sha512_raw(byte[] message, byte[] digest);
 
-    public static native void hmac256(byte[] key, byte[] message, byte[] hmac);
+    static native void hmac256(byte[] key, byte[] message, byte[] hmac);
 
-    public static native void hmac512(byte[] key, byte[] message, byte[] hmac);
+    static native void hmac512(byte[] key, byte[] message, byte[] hmac);
 
-    public static native void pbkdf2_hmac_sha256(byte[] pass,
-                                                 byte[] salt,
-                                                 long iteration,
-                                                 byte[] key);
+    static native void pbkdf2_hmac_sha256(byte[] pass,
+                                          byte[] salt,
+                                          long iteration,
+                                          byte[] key);
 
-    public static native long crc32(byte[] bytes);
+    static native long crc32(byte[] bytes);
 
-    public static native long crc32n(byte[] bytes);
+    static native long crc32n(byte[] bytes);
 }
