b4441a561714230354a5ea9f93b13f17f100deb7
Hiếu Phạm <hieupham@bitmark.com>
2021-01-11 11:18:57 +0700

    better build scripts

--- bip39/java/scripts/build.sh
+++ bip39/java/scripts/build.sh
@@ -7,9 +7,6 @@ source scripts/helper.sh
 echo "${JAVA_HOME:?}"
 echo "${CC:?}"
 
-echo 'Cleanup...'
-./scripts/cleanup.sh
-
 PARENT_ROOT_DIR=$(
   cd ../..
   pwd
@@ -21,34 +18,59 @@ if ! is_osx; then
   LIB_NAME="libbc-bip39-jni.so"
   JNI_MD_DIR="linux"
 fi
+BUILD_LOG_DIR="log"
+BUILD_LOG="$BUILD_LOG_DIR/$(date +%s)-log.txt"
 
-# Install bc-crypto-base
-pushd "$PARENT_ROOT_DIR/deps/bc-crypto-base"
-./configure
-make clean
-make CFLAGS=-fPIC check
-make CFLAGS=-fPIC install
-popd
+clean_up() {
+  ./scripts/cleanup.sh
+}
 
-# Install bc-bip39
-pushd "$PARENT_ROOT_DIR/deps/bc-bip39"
-./configure
-make clean
-make CFLAGS=-fPIC check
-popd
+build_bc_crypto_base() {
+  pushd "$PARENT_ROOT_DIR/deps/bc-crypto-base"
+  ./configure
+  make clean
+  make CFLAGS=-fPIC check
+  make CFLAGS=-fPIC install
+  popd
+}
 
-# Install jni lib
-echo "Building $LIB_NAME..."
-mkdir -p $OUT_DIR
-$CC -I"$JAVA_HOME/include" \
-  -I"$JAVA_HOME/include/$JNI_MD_DIR" \
-  -I"$PARENT_ROOT_DIR/base-jni" \
-  -I"$PARENT_ROOT_DIR/deps/bc-bip39/src" \
-  -fexceptions -frtti -shared -fPIC \
-  src/main/jniLibs/*.c \
-  "$PARENT_ROOT_DIR"/base-jni/*.c \
-  "$PARENT_ROOT_DIR"/deps/bc-bip39/src/libbc-bip39.a \
-  "$PARENT_ROOT_DIR"/deps/bc-crypto-base/src/libbc-crypto-base.a \
-  -o \
-  $OUT_DIR/$LIB_NAME
-echo "Done. Checkout the release file at $OUT_DIR/$LIB_NAME"
+build_bc_bip39() {
+  pushd "$PARENT_ROOT_DIR/deps/bc-bip39"
+  ./configure
+  make clean
+  make CFLAGS=-fPIC check
+  popd
+}
+
+build_jni() {
+  mkdir -p $OUT_DIR
+  $CC -I"$JAVA_HOME/include" \
+    -I"$JAVA_HOME/include/$JNI_MD_DIR" \
+    -I"$PARENT_ROOT_DIR/base-jni" \
+    -I"$PARENT_ROOT_DIR/deps/bc-bip39/src" \
+    -fexceptions -frtti -shared -fPIC \
+    src/main/jniLibs/*.c \
+    "$PARENT_ROOT_DIR"/base-jni/*.c \
+    "$PARENT_ROOT_DIR"/deps/bc-bip39/src/libbc-bip39.a \
+    "$PARENT_ROOT_DIR"/deps/bc-crypto-base/src/libbc-crypto-base.a \
+    -o \
+    $OUT_DIR/$LIB_NAME
+}
+
+(
+  mkdir -p ${BUILD_LOG_DIR}
+  echo -n >"${BUILD_LOG}"
+
+  echo 'Cleanup...'
+  clean_up
+
+  echo 'Building bc-crypto-base...'
+  build_bc_crypto_base
+
+  echo 'Building bc-bip39...'
+  build_bc_bip39
+
+  echo "Building $LIB_NAME..."
+  build_jni
+  echo "Done. Checkout the release file at $OUT_DIR/$LIB_NAME"
+) | tee "${BUILD_LOG}"
--- shamir/java/scripts/build.sh
+++ shamir/java/scripts/build.sh
@@ -4,9 +4,6 @@ set -e
 
 source scripts/helper.sh
 
-echo 'Cleanup...'
-./scripts/cleanup.sh
-
 echo "${JAVA_HOME:?}"
 echo "${CC:?}"
 
@@ -21,34 +18,59 @@ if ! is_osx; then
   LIB_NAME="libbc-shamir-jni.so"
   JNI_MD_DIR="linux"
 fi
+BUILD_LOG_DIR="log"
+BUILD_LOG="$BUILD_LOG_DIR/$(date +%s)-log.txt"
 
-# Install bc-crypto-base
-pushd "$PARENT_ROOT_DIR/deps/bc-crypto-base"
-./configure
-make clean
-make CFLAGS=-fPIC check
-make CFLAGS=-fPIC install
-popd
+clean_up() {
+  ./scripts/cleanup.sh
+}
 
-# Install bc-shamir
-pushd "$PARENT_ROOT_DIR/deps/bc-shamir"
-./configure
-make clean
-make CFLAGS=-fPIC check
-popd
+build_bc_crypto_base() {
+  pushd "$PARENT_ROOT_DIR/deps/bc-crypto-base"
+  ./configure
+  make clean
+  make CFLAGS=-fPIC check
+  make CFLAGS=-fPIC install
+  popd
+}
 
-# Install jni lib
-echo "Building $LIB_NAME..."
-mkdir -p $OUT_DIR
-$CC -I"$JAVA_HOME/include" \
-  -I"$JAVA_HOME/include/$JNI_MD_DIR" \
-  -I"$PARENT_ROOT_DIR/base-jni" \
-  -I"$PARENT_ROOT_DIR/deps/bc-shamir/src" \
-  -fexceptions -frtti -shared -fPIC \
-  src/main/jniLibs/*.c \
-  "$PARENT_ROOT_DIR"/base-jni/*.c \
-  "$PARENT_ROOT_DIR"/deps/bc-shamir/src/libbc-shamir.a \
-  "$PARENT_ROOT_DIR"/deps/bc-crypto-base/src/libbc-crypto-base.a \
-  -o \
-  $OUT_DIR/$LIB_NAME
-echo "Done. Checkout the release file at $OUT_DIR/$LIB_NAME"
+build_bc_shamir() {
+  pushd "$PARENT_ROOT_DIR/deps/bc-shamir"
+  ./configure
+  make clean
+  make CFLAGS=-fPIC check
+  popd
+}
+
+build_jni() {
+  mkdir -p $OUT_DIR
+  $CC -I"$JAVA_HOME/include" \
+    -I"$JAVA_HOME/include/$JNI_MD_DIR" \
+    -I"$PARENT_ROOT_DIR/base-jni" \
+    -I"$PARENT_ROOT_DIR/deps/bc-shamir/src" \
+    -fexceptions -frtti -shared -fPIC \
+    src/main/jniLibs/*.c \
+    "$PARENT_ROOT_DIR"/base-jni/*.c \
+    "$PARENT_ROOT_DIR"/deps/bc-shamir/src/libbc-shamir.a \
+    "$PARENT_ROOT_DIR"/deps/bc-crypto-base/src/libbc-crypto-base.a \
+    -o \
+    $OUT_DIR/$LIB_NAME
+}
+
+(
+  mkdir -p ${BUILD_LOG_DIR}
+  echo -n >"${BUILD_LOG}"
+
+  echo 'Cleanup...'
+  clean_up
+
+  echo 'Building bc-crypto-base...'
+  build_bc_crypto_base
+
+  echo 'Building bc-shamir...'
+  build_bc_shamir
+
+  echo "Building $LIB_NAME..."
+  build_jni
+  echo "Done. Checkout the release file at $OUT_DIR/$LIB_NAME"
+) | tee "${BUILD_LOG}"
